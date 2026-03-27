aade3d8f370e64d813eb5974df97590c281b2c22
Ivo Teruggi <ivoteruggi@hotmail.com>
2026-03-27 10:51:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ const routes = require('./routes/routes');
 // Use routes
 app.use('/', routes);
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running and accepting outside connections on port ${PORT}`);
 });
